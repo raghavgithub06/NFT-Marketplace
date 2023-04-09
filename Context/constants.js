4790c1aea51fe07fa35472e31d2f1dae6454a8ed
Raghav Singh <raghavksingh750@gmail.com>
2023-04-09 20:54:47 +0530

    first commit

---
+++ Context/constants.js
@@ -0,0 +1,6 @@
+///0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+
+import nftMarketplace from "./NFTMarketplace.json";
+
+export const NFTMarketplaceAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+export const NFTMarketplaceABI = nftMarketplace.abi;
